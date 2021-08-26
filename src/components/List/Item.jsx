53dca5509bbc30aba5eb5a4ec37a49e7922dfac5
Bugrahan Vural <bugrahan.vural@faraday.net>
2021-08-26 14:08:35 +0300

    mergeing

--- src/components/List/Item.jsx
+++ src/components/List/Item.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
+import Dropdown from "react-dropdown";
+import "react-dropdown/style.css";
 
 function Item(props) {
-return (
+  const [isClicked, setClicked] = useState(false);
+  const options = ["one", "two", "three"];
+  const defaultOption = options[0];
+
+  const handleClick = () => {
+    setClicked(!isClicked);
+  };
+
+  function showButton() {
+    if (isClicked) {
+      return (
+        <Dropdown
+          options={options}
+          value={defaultOption}
+          placeholder="Select an option"
+        />
+      );
+    }
+  }
+
+  return (
     <div className="Item">
-        <img src={props.icon} alt="Item_icon"></img>
+      <img src={props.icon} alt="Item_icon"></img>
+      <div className="info">
         <h1>{props.title}</h1>
         <p>{props.description}</p>
+      </div>
+
+      <button onClick={handleClick} className="itemBtn">
+        :
+      </button>
+      {showButton()}
     </div>
-);
+  );
 }
 
 export default Item;
