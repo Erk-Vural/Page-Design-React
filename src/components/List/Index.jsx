f9d983ed31e98f70ee894c80a6af9128af626de7
Bugrahan Vural <bugrahan.vural@faraday.net>
2021-08-26 14:13:41 +0300

    added detail button to item

--- src/components/List/Index.jsx
+++ src/components/List/Index.jsx
@@ -8,15 +8,47 @@ import list from "../../list";
 
 function List() {
   const [layout, setLayout] = useState("Grid");
-  const [page, setPage] = React.useState(1);
+  const [page, setPage] = useState(1);
+  const [hidden, setHidden] = useState(false);
   let itemLimit = 5;
 
   const handleChange = (event, value) => {
     setPage(value);
   };
 
-  function getLayout(newLayout) {
+  const getLayout = (newLayout) => {
     setLayout(newLayout);
+  };
+
+  const getHidden = () => {
+    setHidden(!hidden);
+  };
+
+  function showList() {
+    if (hidden) {
+      return (
+        <div>
+          <p>The list is hidden.</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          {getTotalResult()}
+
+          <div className="listItems">{listItems()}</div>
+
+          <div className="pagination">
+            <Pagination
+              count={list.length}
+              color="primary"
+              page={page}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+      );
+    }
   }
 
   function getTotalResult() {
@@ -45,28 +77,15 @@ function List() {
     });
   }
 
-  function hideList() {}
-
   return (
     <div className={layout}>
       <div className="hideList">
-        <button onClick={hideList}>Hide List</button>
+        <button onClick={getHidden}>Hide List</button>
       </div>
 
       <Options getLayout={getLayout} />
 
-      {getTotalResult()}
-
-      <div className="listItems">{listItems()}</div>
-
-      <div className="pagination">
-        <Pagination
-          count={list.length}
-          color="primary"
-          page={page}
-          onChange={handleChange}
-        />
-      </div>
+      {showList()}
     </div>
   );
 }
--- src/components/Venue/Index.jsx
+++ src/components/Venue/Index.jsx
@@ -18,7 +18,7 @@ function Venue() {
 
   return (
     <div className="Venue">
-      <h1>Venue</h1>
+       <h1>Venue</h1>
       <Arrange />
       {Map}
       <List />
